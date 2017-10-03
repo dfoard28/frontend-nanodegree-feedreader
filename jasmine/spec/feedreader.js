fc2ea418eeb1fb35a1764ec7c281611ad77d4127
Desiree Foard <foard.desiree@gmail.com>
2017-10-03 10:52:01 -0600

    added in the menu suite and also a spec to check if the body has a class of menu-hidden to begin.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -55,12 +55,17 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
+    describe('The menu',function(){
+         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('should not show menu',function(){
+            var body = document.getElementsByTagName('body');
+            expect(body[0].className).toMatch('menu-hidden');
+        })  
+    })
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
